e8357fcc8c8e90ae2ace1b7eb16710bf0664ad23
Kunal Chandan <kunal_chandan@outlook.com>
2024-01-23 23:28:55 -0800

    Add save/load, buttons, Update UI Draw

--- canvas.py
+++ canvas.py
@@ -1,23 +1,26 @@
+import pickle
 import threading
-import time
-import simpleaudio as sa
+
 import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.filedialog as fd
+
+import simpleaudio as sa
 import numpy as np
 import noise
 
-HEIGHTS = np.zeros((noise.NUM_FREQS))
-
+CANVAS_HEIGHT = 500
+HEIGHTS = np.zeros((noise.NUM_FREQS)) + CANVAS_HEIGHT
+DB = np.zeros((noise.NUM_FREQS))
 
 def play_sound():
-    sound = noise.gen_noise()
+    sound = noise.gen_noise(HEIGHTS)
     sound = (sound * (2**15)).astype("int16")
     wa = sa.WaveObject(sound, 1, 2, noise.SAMPLING_RATE)
-    while True:
-        play_obj = wa.play()
-        play_obj.wait_done()
-        time.sleep(3)
-        # play_obj = sa.play_buffer(sound, 1, 2, noise.SAMPLING_RATE)
-        # play_obj.wait_done()
+    play_obj = wa.play()
+    play_obj.wait_done()
+    # play_obj = sa.play_buffer(sound, 1, 2, noise.SAMPLING_RATE)
+    # play_obj.wait_done()
 
 
 class AmpPlot(tk.Canvas):
@@ -25,49 +28,119 @@ class AmpPlot(tk.Canvas):
 
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
+        self.pack(fill=tk.BOTH, expand=True)
+        self.width = kwargs.get("width", 100)
+        self.height = kwargs.get("height", 100)
         # self.bind("<Button-1>", self.save_posn)
         self.bind("<B1-Motion>", self.add_line)
         self.bind("P", play_sound)
+        self.draw_UI()
 
     def draw_UI(self):
-        self.create_line(20, 0, 20, self.winfo_height(), fill="red")
-        n_ticks = 20
+        """Draw the UI e.g. dB lines, freq lines"""
+        self.left_ax = 20
+        self.create_line(self.left_ax, 0, self.left_ax, self.height, fill="red")
+        n_ticks = 10
         for i in range(n_ticks):
-            y_pos = i * (self.winfo_height() / n_ticks)
-            self.create_line(20, y_pos, 20 + 30, y_pos, fill="red")
+            y_pos = i * (self.height / n_ticks)
+            self.create_line(self.left_ax, y_pos, self.left_ax + 30, y_pos, fill="red")
             if i > 0:
-                self.create_text(20 + 50, y_pos, text=f"{(i-2)*20:02}dB")
-        bottom_bar_height = self.winfo_height() - (self.winfo_height() / 20)
+                db = (-i + 2) * 20
+                self.create_text(self.left_ax + 50, y_pos, text=f"{db:02}dB")
+        self.bottom_bar_height = self.height - (self.height / n_ticks)
         self.create_line(
-            20, bottom_bar_height, self.winfo_width(), bottom_bar_height, fill="black"
+            self.left_ax,
+            self.bottom_bar_height,
+            self.width,
+            self.bottom_bar_height,
+            fill="black",
         )
+        for i in range(n_ticks):
+            x_pos = i * (self.width / n_ticks) + self.left_ax
+            self.create_line(
+                x_pos, self.bottom_bar_height, x_pos, self.bottom_bar_height + 10
+            )
+            self.create_text(
+                x_pos,
+                self.bottom_bar_height + 10,
+                text=f"{noise.freqs_hz[int(x_pos)]:.1e}Hz",
+            )
 
-    def play_sound(self, event):
+    def play_sound(self, event=None):
+        """Play sample of generated sound"""
         print(f"You pressed the {event} key!")
-        # sound_thread = threading.Thread(target=play_sound)
-        # sound_thread.start()
-        # sound_thread.join()
-
-    # def save_posn(self, event):
-    #     self.lastx, self.lasty = event.x, event.y
+        sound_thread = threading.Thread(target=play_sound)
+        sound_thread.start()
 
     def add_line(self, event):
-        delete_line = (event.x, self.winfo_height(), event.x, 0)
-        new_line = (event.x, self.winfo_height(), event.x, event.y)
-        HEIGHTS[event.x] = event.y
-        self.create_line(delete_line, fill="white")
-        self.create_line(new_line)
-        self.draw_UI()
-        # self.save_posn(event)
+        """Draw heights and change heights"""
+        if (
+            event.x > self.left_ax
+            and event.y < self.bottom_bar_height
+            and event.x < noise.NUM_FREQS
+        ):
+            delete_line = (
+                event.x,
+                self.bottom_bar_height,
+                event.x,
+                min(HEIGHTS[event.x], self.bottom_bar_height),
+            )
+            new_line = (event.x, self.bottom_bar_height, event.x, event.y)
+            HEIGHTS[event.x] = event.y
+            DB[event.x] = HEIGHTS # Translate height to db
+            self.create_line(delete_line, fill="white")
+            self.create_line(new_line)
+            # self.save_posn(event)
+
+
+def save_data():
+    """Save with a dialog"""
+    f = fd.asksaveasfilename(
+        confirmoverwrite=False,
+        initialdir="./",
+        defaultextension=".pkl",
+        initialfile="heights",
+        filetypes=(("Pickle file", ".pkl .pickle"), ("All Files", "*.*")),
+    )
+    with open(f, "wb") as file_handle:
+        pickle.dump(HEIGHTS, file_handle)
+
+
+def load_data():
+    """Load with a dialog"""
+    global HEIGHTS
+    f = fd.askopenfilename(
+        initialdir="./",
+        defaultextension=".pkl",
+        initialfile="heights",
+        filetypes=(("Pickle file", ".pkl .pickle"), ("All Files", "*.*")),
+    )
+    with open(f, "rb") as file_handle:
+        HEIGHTS = pickle.load(file_handle)
+    # Redraw UI
 
 
 def create_window():
     root = tk.Tk()
+    root.title("MellowDy")
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
+    root.geometry(f"{noise.NUM_FREQS}x{600}")
 
-    sketch = AmpPlot(root, width=noise.NUM_FREQS, height=500, bg="white")
-    sketch.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+    sketch = AmpPlot(root, width=noise.NUM_FREQS, height=CANVAS_HEIGHT, bg="white")
+    sketch.grid(column=0, row=0)
+    sketch.draw_UI()
+
+    buttons_frame = tk.Frame(root)
+    play = ttk.Button(buttons_frame, text="Play Sound", command=sketch.play_sound)
+    save = ttk.Button(buttons_frame, text="Save", command=save_data)
+    load = ttk.Button(buttons_frame, text="Load", command=load_data)
+    redraw = ttk.Button(buttons_frame, text="Redraw UI", command=sketch.draw_UI)
+    buttons_frame.grid(column=0, row=1)
+    play.grid(column=0, row=0)
+    save.grid(column=1, row=0)
+    load.grid(column=2, row=0)
+    redraw.grid(column=3, row=0)
 
     root.mainloop()
 
