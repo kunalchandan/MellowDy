5f26776a9edef1c77d11b4c528ca9ad5a4991fb0
Kunal Chandan <kunal_chandan@outlook.com>
2024-01-24 01:10:36 -0800

    Faster noise calc

--- noise.py
+++ noise.py
@@ -1,9 +1,9 @@
+import time
 import numpy as np
-from scipy.io.wavfile import write
 
 NUM_FREQS = 1000
 
-freqs_hz = np.geomspace(1, 10_000, NUM_FREQS)  # frequencies Hz
+freqs_hz = np.geomspace(1, 20_000, NUM_FREQS)  # frequencies Hz
 freqs_rad_s = freqs_hz * 2 * np.pi  # \omega = 2 \pi f
 
 amplitudes = np.zeros((NUM_FREQS)) + 0.1 + np.sin(np.linspace(0, np.pi, NUM_FREQS))
@@ -11,20 +11,27 @@ phase = np.random.random((NUM_FREQS)) * np.pi * 2
 
 SAMPLING_RATE = 44100
 DT = 1.0 / SAMPLING_RATE
-SAMPLES = SAMPLING_RATE * 3
+SAMPLES = SAMPLING_RATE * 1
 
 
-def gen_noise() -> np.array:
+def gen_noise(response: np.array) -> np.array:
     noise = np.zeros(SAMPLES, dtype=np.float32)  # white noise
 
-    t = 0.0
-    for i in range(SAMPLES):
-        t += DT
-        vol = np.sum(np.multiply(np.sin(freqs_rad_s * t + phase), amplitudes))
-        noise[i] = vol
+    time_vals = np.array(range(SAMPLES)) / SAMPLING_RATE
+    # vol_calculation = lambda time: np.sum(np.multiply(np.sin(freqs_rad_s * time + phase), response))
+    # vol_vectorized = np.vectorize(vol_calculation)
+    t1 = time.time_ns()
+    vol = [
+        np.sum(np.multiply(np.sin(freqs_rad_s * t + phase), response))
+        for t in time_vals
+    ]
+    t2 = time.time_ns()
+    print(f"{(t2-t1)//1000_000}ms")
+    # t1 = time.time_ns()
+    # vol = np.sum(np.multiply(np.sin(np.outer(time_vals, freqs_rad_s) + phase), response))
+    # t2 = time.time_ns()
+    # print(f"{(t2-t1)//1000_000}ms")
 
-    # noise /= np.max(np.abs(noise), axis=0)
-    noise /= 1000
-
-    # write("noise.wav", SAMPLING_RATE, noise)
+    noise = vol
+    noise /= np.max(np.abs(noise), axis=0)
     return noise
